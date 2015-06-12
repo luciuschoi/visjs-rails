7f939519e1f4551f3713e4e244b53a39363d6d0f
Hyo Seong Choi <lucius.choi@gmail.com>
2015-06-12 16:41:22 +0900

    Initial commit

---
+++ lib/visjs/rails.rb
@@ -0,0 +1,8 @@
+require "visjs/rails/version"
+
+module Visjs
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/visjs/rails/version.rb
@@ -0,0 +1,5 @@
+module Visjs
+  module Rails
+    VERSION = "4.2.0.0"
+  end
+end
